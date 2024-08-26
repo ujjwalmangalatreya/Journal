0d2085960f5da57e717f669aee32fb2ca84af01e
Ujjwal <ujjwalmangalatreya@gmail.com>
2024-08-26 12:34:52 +0545

    1. MongoDB Atlas added 2. Added connection string 3. addred @Transactions 4. Created Platform transaction Manager

--- src/main/java/com/project/JournalApplication/service/JournalEntryService.java
+++ src/main/java/com/project/JournalApplication/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.project.JournalApplication.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,11 +19,18 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        User user = userService.findByUserName(userName);
-        JournalEntry savedJournalEntries = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(savedJournalEntries);
-        userService.saveEntry(user);
+        try{
+            User user = userService.findByUserName(userName);
+            JournalEntry savedJournalEntries = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(savedJournalEntries);
+            userService.saveEntry(user);
+        }catch (Exception err){
+            System.out.println(err);
+            throw new RuntimeException("An Error occurred when saving the entry ");
+        }
+
     }
     public void saveEntry(JournalEntry journalEntry){
         journalEntryRepository.save(journalEntry);
